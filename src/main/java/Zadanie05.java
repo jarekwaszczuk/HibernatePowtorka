3aca415853a0a4b063da2b3fcf5ae8ff01d2ff72
Jarosław Waszczuk <jarekwaszczuk@poczta.fm>
2018-08-11 11:40:26 +0200

    Zadanie05 set z ROOM w FACULTY

--- src/main/java/Zadanie05.java
+++ src/main/java/Zadanie05.java
@@ -5,6 +5,7 @@ import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import util.HibernateUtils;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Zadanie05 {
@@ -26,5 +27,11 @@ public class Zadanie05 {
 
         System.out.println(resultList);
         System.out.println("Rekordów: " + resultList.size());
+        //TODO przerobić poniższe na forEach
+        System.out.println(resultList.get(0).getRooms());
+        System.out.println("Ile pokoi: " + resultList.get(0).getRooms().size() + " (" + resultList.get(1).getName() + ")");
+        System.out.println(resultList.get(1).getRooms());
+        System.out.println("Ile pokoi: " + resultList.get(1).getRooms().size() + " (" + resultList.get(1).getName() + ")");
+
     }
 }
